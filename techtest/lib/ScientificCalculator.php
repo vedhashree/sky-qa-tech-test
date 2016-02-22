2c852b946de4817476eec4e4197c0ae679f99a33
vedhashree <vedhashree87@gmail.com>
2016-02-22 18:13:09 +0000

    Added files via upload

---
+++ techtest/lib/ScientificCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+class ScientificCalculator extends Calculator implements iScientificCalculator {
+
+	public function cubeRoot($a){
+		return pow($a, 1/3);
+	}
+	public function factorial($a){
+		$factorial = 1;
+		for ($a=$num; $a>=1; $a--)
+		{
+			$factorial = $factorial * $a;
+		}
+		return $factorial;
+	}
+	public function decToHex($a){
+		return dechex($a);
+	}
+}
